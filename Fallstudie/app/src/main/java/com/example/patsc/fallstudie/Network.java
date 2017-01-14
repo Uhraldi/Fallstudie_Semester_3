c95823b05ce912e0ec05a23e1e0623a8e7c69ee1
Vincent <vincent.schmalor@gmail.com>
2017-01-14 19:28:36 +0100

    Networktesting...

--- Fallstudie/app/src/main/java/com/example/patsc/fallstudie/Network.java
+++ Fallstudie/app/src/main/java/com/example/patsc/fallstudie/Network.java
@@ -1,118 +1,63 @@
 package com.example.patsc.fallstudie;
 
-
-import android.provider.ContactsContract;
-
-import com.example.patsc.fallstudie.Bestandteile.Uhrwerk;
+import android.os.AsyncTask;
 import com.google.gson.Gson;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import org.json.JSONObject;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-
 
 /**
  * Created by vince on 13/01/2017.
  */
 
-public class Network {
+public class Network extends AsyncTask{
 
-    Uhrwerk u = new Uhrwerk();
+    public static String testURL = "testurl";
     Gson gson = new Gson();
+    final JSONObject root = new JSONObject();
 
-    public void
-
-    /**
-     * Wird verwendet, um die Daten zu versenden
-     */
-    private void sendConnect(){
-        URL url = null;
-        HttpURLConnection client = null;
-        OutputStream outputPost = null;
+    public String toJsonOnConnect(String spielername){
         try {
-            url = new URL("localhost:8080/api/connect");
-            client = (HttpURLConnection) url.openConnection();
-            client.setRequestMethod("POST");
-            client.setDoOutput(true);
-            outputPost = new BufferedOutputStream(client.getOutputStream());
-            String a = "";
-            outputPost.write(a.getBytes());
-            outputPost.flush();
-            outputPost.close();
-        }catch (Exception e){
+            root.put("status", "connect");
+            root.put("id", spielername);
+        }catch(Exception e){
             e.printStackTrace();
-        }finally {
-            if (client != null){
-                client.disconnect();
-            }
         }
+        return root.toString();
     }
 
-    public void sendReady(Daten daten){
-        URL url = null;
-        HttpURLConnection client = null;
-        OutputStream outputPost = null;
-        try {
-            url = new URL("localhost:8080/api/ready");
-            client = (HttpURLConnection) url.openConnection();
-            client.setRequestMethod("POST");
-            client.setDoOutput(true);
-            outputPost = new BufferedOutputStream(client.getOutputStream());
-            String a = gson.toJson(daten);
-            outputPost.write(a.getBytes());
-            outputPost.flush();
-            outputPost.close();
-        }catch (Exception e){
-            e.printStackTrace();
-        }finally {
-            if (client != null){
-                client.disconnect();
+    public void sendDataToServer(String toSend){
+         final String json = toSend;
+
+        new AsyncTask<Void, Void, String>(){
+
+            @Override
+            protected String doInBackground(Void... voids) {
+                return getServerResponse(json);
             }
-        }
+
+            @Override
+            protected void onPostExecute(String s) {
+                return result;
+            }
+        }.execute();
     }
 
-    public void sendReadyReq(){
+    private String getServerResponse(String json) {
         try {
-
-            URL url = new URL("http://localhost:8080/api/ready");
+            URL url = new URL(Network.testURL);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.setRequestProperty("Accept", "application/json");
 
-            /*
-            if (conn.getResponseCode() != 200) {
-                throw new RuntimeException("Failed : HTTP error code : "
-                        + conn.getResponseCode());
-            }
-            */
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(
-                    (conn.getInputStream())));
-
-            Daten output = null;
-            while ((output = gson.fromJson(br.readLine())) != null) {
-                System.out.println(output);
-            }
-
-            conn.disconnect();
-
-        } catch (MalformedURLException e) {
-
+        }catch (Exception e){
             e.printStackTrace();
-
-        } catch (IOException e) {
-
-            e.printStackTrace();
-
         }
+    }
 
+
+    @Override
+    protected Object doInBackground(Object[] objects) {
+        return null;
     }
 }
 
